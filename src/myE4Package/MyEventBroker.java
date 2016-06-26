59e1873a7978f16dc1fa2a42a339282adac67a27
tfdllc100 <tfdllc100@TFDL-pc10118.uc.ucalgary.ca>
2016-06-26 13:10:10 -0600

    Code to post every 30 seconds using eventbroker.  Still have to figure out to run this class when product is launched.

--- src/myE4Package/MyEventBroker.java
+++ src/myE4Package/MyEventBroker.java
@@ -1,12 +1,28 @@
 package myE4Package;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 
-import org.eclipse.e4.core.contexts.IEclipseContext;
+//import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.services.events.IEventBroker;
 
 public class MyEventBroker {
-	@Inject
-	IEclipseContext eclipseContext;
-//	@Inject
-//	IEventBroker eventBroker;
+	
+//		@Inject
+//		IEclipseContext eclipseContext;
+		@Inject
+		private static IEventBroker eventBroker;
+		
+		public static void main(String[] args) {
+			while (true) {
+				try {
+					TimeUnit.SECONDS.sleep(30);
+				} catch(InterruptedException ex) {
+					System.out.println("Exception on sleep: " + ex);
+				}
+				boolean wasDispatchedSuccessfully = eventBroker.post(MyEventConstants.TOPIC_TODO_NEW, "text");
+				System.out.println("post result " + wasDispatchedSuccessfully);
+			}
+		}
 }
---
+++ src/myE4Package/MyEventConstants.java
@@ -0,0 +1,5 @@
+package myE4Package;
+
+public final class MyEventConstants {
+	public static final String TOPIC_TODO_NEW = "TOPIC_TODO_NEW";
+}
